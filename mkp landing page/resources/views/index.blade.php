8f05c2f571b36cabf8b80ba02dd4b7056fe31a4d
nandareborn98 <intanaprilioni99@gmail.com>
2019-12-01 17:04:40 +0700

    Mkp Landing Page
    version 1

---
+++ mkp landing page/resources/views/index.blade.php
@@ -0,0 +1,528 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>Multimedia Kampus Pink</title>
+
+		<link href="css/bootstrap.min.css" rel='stylesheet' type='text/css' />
+		<link href="css/bootstrap.css" rel='stylesheet' type='text/css' />
+		<script type="text/javascript" src="js/bootstrap.js"></script>
+		<script type="text/javascript" src="js/bootstrap.min.js"></script>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+		</script>
+    	<link href='http://fonts.googleapis.com/css?family=Raleway:400,100,200,300,500,600,700,800,900' rel='stylesheet' type='text/css'>
+    	<link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
+		<!--  jquery plguin -->
+	    <script type="text/javascript" src="js/jquery.min.js"></script>
+		<!--start slider -->
+	    <link rel="stylesheet" href="css/fwslider.css" media="all">
+		<script src="js/jquery-ui.min.js"></script>
+		<script src="js/css3-mediaqueries.js"></script>
+		<script src="js/fwslider.js"></script>
+	<!--end slider -->
+	 <script type="text/javascript">
+			$(document).ready(function() {
+			
+				var defaults = {
+		  			containerID: 'toTop', // fading element id
+					containerHoverID: 'toTopHover', // fading element hover id
+					scrollSpeed: 1200,
+					easingType: 'linear' 
+		 		};
+				
+				
+				$().UItoTop({ easingType: 'easeOutQuart' });
+				
+			});
+		</script>
+		<!-- start testimonials -->
+		<!-- da-slider -->
+		<link rel="stylesheet" type="text/css" href="css/slider.css" />
+		<script type="text/javascript" src="js/modernizr.custom.28468.js"></script>
+<script type="text/javascript" src="js/jquery.cslider.js"></script>
+	<script type="text/javascript">
+			$(function() {
+
+				$('#da-slider').cslider({
+					autoplay : true,
+					bgincrement : 450
+				});
+
+			});
+		</script>
+		<script type="text/javascript">
+			$(function() {
+
+				$('#da-slider1').cslider({
+					autoplay : true,
+					bgincrement : 450
+				});
+
+			});
+		</script>
+
+
+</head>
+<body>
+<!-- start header -->
+<div class="header_bg">
+	<!-- start header -->
+<div class="header_bg">
+	<!-----start-conatiner---->
+		<div class="container">
+			<!-----start-header---->
+			<div class="header">
+					<!----start-top-nav---->
+		      <header id="topnav">
+			        <nav>
+			        		 <ul>
+								<li class=""><a  href="#home">Beranda</a></li>
+								<li><a href="#about" class="scroll">Tentang</a></li>
+								<li><a href="#portfolio" class="scroll">Portfolio</a></li>
+								<li><a href="#blog" class="scroll">Kumpul Akrab</a></li>
+								<li class="last"><a href="#contact" class="scroll">Contact</a></li>
+								<div class="clearfix"> </div>
+							</ul>
+		        	</nav>
+			         <h1><a href="index.html"><img src="images/logomkp1.png" alt=""/></a></h1>
+			        	<a href="#" id="navbtn">Nav Menu</a>
+			       <div class="clearfix"> </div>
+		        </header>
+		         <!----start-top-nav---->
+		    </div>
+		   <!-----start-header---->
+		</div>
+	</div>
+		<script type="text/javascript"  src="js/menu.js"></script>
+</div>
+<!----start-images-slider---->
+		<div class="images-slider">
+			<!-- start slider -->
+		    <div id="fwslider">
+		        <div class="slider_container">
+		            <div class="slide"> 
+		                <!-- Slide image -->
+		                    <img src="images/background.jpg" alt=""/>
+		                <!-- /Slide image -->
+		                <!-- Texts container -->
+		                <div class="slide_content">
+		                    <div class="slide_content_wrap">
+		                        <!-- Text title -->
+		                        <h4 class="title">Multimedia <span>Kampus</span> Pink<br>
+		                        </h4>
+		                        <!-- /Text title -->
+		                        
+		                        <div class="slide-btns description">	                      
+		                        </div>
+		                    </div>
+		                </div>
+		                 <!-- /Texts container -->
+		            </div>
+		            <!-- /Duplicate to create more slides -->
+		            <div class="slide">
+		                <img src="images/background1.jpg" alt=""/>
+		                 <div class="slide_content">
+		                    <div class="slide_content_wrap">
+		                        <!-- Text title -->
+		                        <h4 class="title">Multimedia <span>Kampus</span> Pink<br>
+		                        </h4>
+		                        <!-- /Text title -->
+		                        <div class="slide-btns description">	                      
+		                        </div>
+		                    </div>
+		                </div>
+		            </div>
+		            <div class="slide">
+		                <img src="images/background1.jpg" alt=""/>
+		                 <div class="slide_content">
+		                    <div class="slide_content_wrap">
+		                        <!-- Text title -->
+		                        <h4 class="title">Multimedia <span>Kampus</span> Pink<br>
+		                        </h4>
+		                        <!-- /Text title -->
+		                        <div class="slide-btns description">	                      
+		                        </div>
+		                    </div>
+		                </div>
+		            </div>
+		            <!--/slide -->
+		        </div>
+		        <div class="timers"> </div>
+		        <div class="slidePrev"><span> </span></div>
+		        <div class="slideNext"><span> </span></div>
+		    </div>
+		    <!--/slider -->
+		</div>
+<!-- about -->
+		<div class="about" id="about">
+	    	<div class="container">
+				<div class="row">
+				    <div class="col-md-12">			
+				        <h3>Tentang Multimedia Kampus Pink</h3>
+				        <span> </span>
+				        <p>Multimedia Kampus Pink Adalah Sebuah UKM yang bergerak di bidang Multimedia dan berdiri sejak tahun 2014. dimana pada saat itu di dirikan oleh para Mahasiswa/ Mahasiswi Stikom yang semangat dalam berkarya di dunia multimedia.</p>
+					</div>
+				</div>		
+			</div>
+		</div>	
+<div class="about-bottom">
+		<div class="about-left pull-left">
+			<h4>Berbagai Kegiatan Mkp</h4>
+			<p> Multimedia Kampus Pink ikut serta dalam beberapa kegiatan dalam kampus maupun 
+			luar kampus dan mengikuti beberapa lomba Tingkat Nasional </p>
+			
+			<p><a href="https://www.instagram.com/mkpstikom/">READ MORE</a></p>
+		</div>	
+		<div class="about-right pull-right">
+			<img src="images/about1.jpg" alt=""/>
+		</div>
+		<div class="clearfix"></div>
+</div>
+<!-- service -->
+<div class="service text-center">
+	<div class="container">
+        <h3>Divisi Multimedia Kampus Pink</h3>
+       <div class="service-bottom">
+	        <div class="row">
+			    <div class="col-md-4">	
+			    	<img src="images/video.png" alt=""/>		
+			        <h4><a href="index.html">Video Production</a></h4>
+			        <p>divisi yang bergerak di bidang video .</p>
+				</div>
+				<div class="col-md-4">	
+					<img src="images/camera.png" alt=""/>		
+			        <h4><a href="index.html">Photoghrapy</a></h4>
+			        <p>divisi yang bergerak di bidang photo.</p>
+				</div>
+				<div class="col-md-4">	
+					<img src="images/computer.png" alt=""/>		
+			        <h4><a href="index.html">Desain Grafis</a></h4>
+			        <p>divisi yang bergerak di bidang Desain Graphic.</p>
+				</div>
+			</div>	
+		</div>	
+   </div>
+</div>
+
+<!-- portfolio -->
+<div class="portfilio" id="portfolio">
+
+<!-- portofolio video production -->
+     <div class="port-text">
+       		<h4>Divisi Video Production</h4>
+       		<p>Beberapa Karya Dari divisi Video Production.</p>
+     </div>	
+	 <div class="port">
+       		<ul>
+					<li><div class="view view-fourth">
+						     <img src="images/port1.jpg" alt=""/>
+							 <div class="mask">	
+							      <div class="border">                   
+									   <h2>PAPERCLIPS</h2>
+									   <span></span>
+									   <p class="a">BRANDING</p>		                       
+								  </div>                                                 
+							 </div>		
+						</div>	
+					</li>
+					<li><div class="view view-fourth">
+						     <img src="images/port2.jpg" alt=""/>
+							 <div class="mask">	
+							      <div class="border">                   
+									   <h2>PAPERCLIPS</h2>
+									   <span></span>
+									   <p class="a">BRANDING</p>		                       
+								  </div>                                                 
+							 </div>		
+						</div>	
+					</li>
+					<li><div class="view view-fourth">
+						     <img src="images/port3.jpg" alt=""/>
+							 <div class="mask">	
+							      <div class="border">                   
+									   <h2>PAPERCLIPS</h2>
+									   <span></span>
+									   <p class="a">BRANDING</p>		                       
+								  </div>                                                 
+							 </div>		
+						</div>	
+					</li>
+					<li><div class="view view-fourth">
+						     <img src="images/port4.jpg" alt=""/>
+							 <div class="mask">	
+							      <div class="border">                   
+									   <h2>PAPERCLIPS</h2>
+									   <span></span>
+									   <p class="a">BRANDING</p>		                       
+								  </div>                                                 
+							 </div>		
+						</div>	
+					</li>
+			</ul>
+			<p><a href="https://www.instagram.com/mkpstikom/">Lihat Semua<span> </span></a></p>	
+			<br>	
+     </div>
+<!-- portofolio video production -->
+
+<!-- portofolio photoghraphy -->
+     <div class="port-text">
+       		<h4>Divisi Photoghrapy</h4>
+       		<p>Beberapa Karya Dari Divisi Photoghrapy.</p>
+     </div>	
+	 <div class="port">
+       		<ul>
+					<li><div class="view view-fourth">
+						     <img src="images/port1.jpg" alt=""/>
+							 <div class="mask">	
+							      <div class="border">                   
+									   <h2>PAPERCLIPS</h2>
+									   <span></span>
+									   <p class="a">BRANDING</p>		                       
+								  </div>                                                 
+							 </div>		
+						</div>	
+					</li>
+					<li><div class="view view-fourth">
+						     <img src="images/port2.jpg" alt=""/>
+							 <div class="mask">	
+							      <div class="border">                   
+									   <h2>PAPERCLIPS</h2>
+									   <span></span>
+									   <p class="a">BRANDING</p>		                       
+								  </div>                                                 
+							 </div>		
+						</div>	
+					</li>
+					<li><div class="view view-fourth">
+						     <img src="images/port3.jpg" alt=""/>
+							 <div class="mask">	
+							      <div class="border">                   
+									   <h2>PAPERCLIPS</h2>
+									   <span></span>
+									   <p class="a">BRANDING</p>		                       
+								  </div>                                                 
+							 </div>		
+						</div>	
+					</li>
+					<li><div class="view view-fourth">
+						     <img src="images/port4.jpg" alt=""/>
+							 <div class="mask">	
+							      <div class="border">                   
+									   <h2>PAPERCLIPS</h2>
+									   <span></span>
+									   <p class="a">BRANDING</p>		                       
+								  </div>                                                 
+							 </div>		
+						</div>	
+					</li>
+			</ul>
+			<p><a href="https://www.instagram.com/mkpphotograpy/">Lihat Semua<span> </span></a></p>	
+			<br>
+     </div>
+<!-- portofolio photoghraphy -->
+
+<!-- portofolio desain grafis -->
+     <div class="port-text">
+       		<h4>Divisi Desain grafis</h4>
+       		<p>Beberapa Karya Dari divisi Desain Grafis.</p>
+     </div>
+     <div class="port">
+       		<ul>
+					<li><div class="view view-fourth">
+						     <img src="images/port1.jpg" alt=""/>
+							 <div class="mask">	
+							      <div class="border">                   
+									   <h2>PAPERCLIPS</h2>
+									   <span></span>
+									   <p class="a">BRANDING</p>		                       
+								  </div>                                                 
+							 </div>		
+						</div>	
+					</li>
+					<li><div class="view view-fourth">
+						     <img src="images/port2.jpg" alt=""/>
+							 <div class="mask">	
+							      <div class="border">                   
+									   <h2>PAPERCLIPS</h2>
+									   <span></span>
+									   <p class="a">BRANDING</p>		                       
+								  </div>                                                 
+							 </div>		
+						</div>	
+					</li>
+					<li><div class="view view-fourth">
+						     <img src="images/port3.jpg" alt=""/>
+							 <div class="mask">	
+							      <div class="border">                   
+									   <h2>PAPERCLIPS</h2>
+									   <span></span>
+									   <p class="a">BRANDING</p>		                       
+								  </div>                                                 
+							 </div>		
+						</div>	
+					</li>
+					<li><div class="view view-fourth">
+						     <img src="images/port4.jpg" alt=""/>
+							 <div class="mask">	
+							      <div class="border">                   
+									   <h2>PAPERCLIPS</h2>
+									   <span></span>
+									   <p class="a">BRANDING</p>		                       
+								  </div>                                                 
+							 </div>		
+						</div>	
+					</li>
+			</ul>	
+			<p><a href="https://www.instagram.com/mkpstikom/">Lihat Semua<span> </span></a></p>
+     </div>
+<!-- portofolio desain grafis -->
+
+</div>
+
+<!-- start kumpul akrab -->
+	<div class="blog" id="blog">
+			<div class="container">
+				<div class="about">	   
+						<div class="row">
+						    <div class="col-md-12">			
+						        <h3>Kumpul Akrab</h3>
+						        <span></span>
+						        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry Lorem Ipsum Lorem Ipsum has been.</p>
+							</div>
+						</div>		
+				</div>	
+				 <div class="blog-bottom">
+			        <div class="blog-left">
+			        	<img src="images/camera.jpg" alt=""/>
+			        </div>
+			        <span class="circle"></span>
+			         <div class="blog-right">       	
+			        	<h6>10 April 2014</h6>
+			        	<h4>Printing and typesetting industry</h4>
+			        	<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled</p>
+			        </div>
+			        <div class="clearfix"></div>
+		         </div>
+		         <div class="blog-bottom">
+			        <div class="blog-left">
+			        	<h6>10 April 2014</h6>
+			        	<h4>Printing and typesetting industry</h4>
+			        	<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled</p>
+			        </div>
+			        <span class="circle"></span>
+		         <div class="blog-right">       	
+		        	<img src="images/camera.jpg" alt=""/>
+		        </div>
+		        <div class="clearfix"></div>
+	         </div>
+	          <div class="blog-bottom">
+		        <div class="blog-left last">
+		        	<img src="images/camera.jpg" alt=""/>
+		        </div>
+		        <span class="circle"> </span>
+		        <div class="blog-right">       	
+		        	<h6>10 April 2014</h6>
+		        	<h4>Printing and typesetting industry</h4>
+		        	<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled</p>
+		        </div>
+		        	<div class="clearfix"> </div>
+		         </div>
+		        <div class="post"><a href="#">All  Posts</a></div>
+			</div>
+	</div>
+<!-- kumpul akrab -->
+
+<!---start-contact---->
+	<div class="contact s4" id="contact">
+		<div class="container">
+			<h3>CONTACT</h3>
+			<div class="row">
+				<div class="col-md-12">
+					<form>
+						<input type="text" class="textbox" value="Name" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Name';}">
+						<input type="text" class="textbox" value="Email" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Email';}">
+						<textarea value="Message" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Message';}">Message</textarea>
+						<input type="submit" value="Submit">
+					</form>
+				</div>
+			</div>
+		</div>
+	</div>
+<!---End-contact---->
+<div class="footer-top">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-3">
+				<a href="index.html"><img src="images/logo1.png" alt=""/></a>
+			    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry</p>
+	        	<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled</p>
+			</div>	
+			<div class="col-md-3">
+				<h4>RECENT POSTS</h4>
+				<div class="foot">
+					<span>24 march 2014</span>
+					<p><a href="#">Printing and typesetting industry</a></p>
+				</div>
+				<div class="foot">
+					<span>24 march 2014</span>
+					<p><a href="#">Printing and typesetting industry</a></p>
+				</div>
+				<div class="foot">
+					<span>24 march 2014</span>
+					<p><a href="#">Printing and typesetting industry</a></p>
+				</div>
+			</div>	
+			<div class="col-md-3">
+				<h4>TWITTER FEEDS</h4>
+				<div class="foot">
+					<span>industry@info.com</span>
+					<p><a href="#">Printing and typesetting industry</a></p>
+				</div>
+				<div class="foot">
+					<span>industry@info.com</span>
+					<p><a href="#">Printing and typesetting industry</a></p>
+				</div>
+				<div class="foot">
+					<span>industry@info.com</span>
+					<p><a href="#">Printing and typesetting industry</a></p>
+				</div>
+			</div>	
+			<div class="col-md-3">
+				<h4 class="a">RECENT POSTS</h4>
+	        	<p class="a">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled</p>
+			    <address>
+			    	<p class="b">Typesetting industry</p>
+			    	<p class="b">Lorem Ipsum has</p>
+			    	<p class="b">Phone : 466,348457,7459</p>
+			    </address>
+			</div>	
+		</div>
+	</div>
+</div>
+<div class="footer">
+	<div class="container">
+		<div class="row">
+ 			<div class="col-md-12">	
+				<div class="copy-right">
+					<p>&#169 Copyright 2014 All Rights Reserved  Template <a href="http://w3layouts.com/">  w3layouts.com</a></p>	
+				</div>	
+			</div>
+		</div>
+    </div>
+</div>
+ <!-- scroll_top_btn -->
+		<script type="text/javascript" src="js/move-top.js"></script>
+		<script type="text/javascript" src="js/easing.js"></script>
+		<script type="text/javascript">
+		jQuery(document).ready(function($) {
+			$(".scroll").click(function(event){		
+				event.preventDefault();
+				$('html,body').animate({scrollTop:$(this.hash).offset().top},1200);
+			});
+		});
+	</script>
+
+		 <a href="#" id="toTop" style="display: block;"><span id="toTopHover" style="opacity: 1;"></span></a>
+</body>
+</html>
